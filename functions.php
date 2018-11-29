c1471d6dcf0af909e659d5ee692d6be7f5c0959a
Jake vincet <jakevincet@gmail.com>
2018-11-29 21:52:57 +0000

    should have fixed index

---
+++ functions.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Age Check</title>
+</head>
+<body>
+<?php
+class checkage
+{
+//public $age;
+
+   public function idCheck($age)
+  {
+    foreach ($age as $ages)
+      {
+  if ($ages >= l_age)
+    {
+    echo nl2br("come on in \n");
+    }
+  else
+    {
+    echo nl2br("Sorry but no \n");
+    }
+      }
+    }
+}
+?>
+</body>
+</html>
